b38d5b7b37ac0dac3b760ec83ce4e08869262062
chen.huaiyu <ast.chen@qq.com>
2019-06-17 19:54:56 +0800

    增加：自动修改每日环比走势；修改：数据填充完后，格式填充逻辑。

--- work2.0/updateP4P.py
+++ work2.0/updateP4P.py
@@ -13,7 +13,7 @@ Created on Mon Jun  3 17:16:26 2019
 """
 
 from sqlalchemy import create_engine
-from datetime import datetime
+from datetime import datetime, timedelta
 from xlwings import constants
 import xlwings as xw
 import pandas as pd
@@ -116,25 +116,49 @@ class Excel(object):
             date_date = datetime.strptime(self._date, '%Y%m%d')
             ll = [i.value for i in sht['A1'].current_region.rows[:1]][0]  # 字段
             sht['A1:A'+str(cntEnd)].rows.autofit()
-            if cntEnd > cntSta:
-                # 汇总列填充
-                sht[cntSta-1, 34:47].api.AutoFill(sht[cntSta-1:cntEnd, 34:47].api,
-                   constants.AutoFillType.xlFillCopy)
-                # 消费列填充
-                sht[cntSta:cntEnd, 47:ll.index(date_date)].value = 0
+            
             # 消费
             sht['A1'].offset(9, ll.index(date_date)).options(transpose=True
                                                              ).value = df_s.values
-            # 格式
-            for i in range(7, 13):
-                sht[cntSta:cntEnd, :ll.index(date_date)+1].api.Borders(i).lineStyle = 1
+               
+            if cntEnd > cntSta:
+                # 填充汇总列
+                sht[cntSta-1, 34:47].api.AutoFill(sht[cntSta-1:cntEnd, 34:47].api,
+                   constants.AutoFillType.xlFillCopy)
+                # 填充消费列
+                sht[cntSta:cntEnd, 47:ll.index(date_date)].value = 0
+                # 填充格式
+                for i in range(7, 13):
+                    sht[cntSta:cntEnd, :ll.index(date_date)+1].api.Borders(i).lineStyle = 1
         except Exception as e:
-            print('产生异常：{}'.format(e))
+            print('toExcel产生异常：{}'.format(e))
             
-    def dailyRatio(sht):
-        
-        pass
-       
+    def dailyRatio(self, sht, dateStr):
+        '''日环比；不计节假日
+        '''
+        date = datetime.strptime(dateStr, '%Y%m%d')
+        if date.weekday() == 6:  # 周日(1)
+            date1 = date - timedelta(2)
+            date2 = date - timedelta(3)
+        elif date.weekday() == 5:  # 周六(7)
+            date1 = date - timedelta(1)
+            date2 = date - timedelta(2)
+        elif date.weekday() == 0:  # 周一(2)
+            date1 = date
+            date2 = date - timedelta(3)
+        elif date.weekday() in [1, 2, 3, 4]:  # 周二、三、四、五(3,4,5,6)
+            date1 = date
+            date2 = date - timedelta(1)
+        # 赋值
+        sht[2, 2].value = '环比增长额\n%s日环比%s日' % (date1.day, date2.day)
+        cnt1 = sht['A3:AJ3'].value.index(date1)
+        cnt2 = sht['A3:AJ3'].value.index(date2)
+        for i in range(3, 19):
+            sht[i, 2].value = sht[i, cnt1].value - sht[i, cnt2].value  # 环比增长额
+            if sht[i, cnt2].value == 0:
+                sht[i, 3].value = 0
+            else:
+                sht[i, 3].value = sht[i, 2].value/sht[i, cnt2].value  # 环比增长率
 
 def main(dateStr):
     # 实例化
@@ -153,8 +177,12 @@ def main(dateStr):
         sht = wb.sheets[shtList[n]]
         # 写入
         ex.toExcel(df, sht)
+    wb.app.calculation = 'automatic'
+    wb.app.calculation = 'manual'
+    # 每日消费走势
+    sht = wb.sheets['每日消费走势']
+    ex.dailyRatio(sht, dateStr)
     # 保存
-    wb.app.calculcation = 'automanul'
     wb.save()
     # wb.close()
     print('\a程序结束')
@@ -163,8 +191,9 @@ if __name__ == '__main__':
     
     # 日期锁定
     star = time.perf_counter()
-    dateStr = pd.date_range(start='20190613', periods=1)
+    dateStr = pd.date_range(start='20190615', periods=2)
     for i in dateStr:
         main(dateStr=i.strftime('%Y%m%d'))
     stop = time.perf_counter()
     print('\a耗时：%.3f min' % ((stop - star)/60))
+    pass
